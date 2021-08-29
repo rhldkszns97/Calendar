2d2de42ba22701aabfcb81bf6c52fdf8fdb58704
지훈 <@DESKTOP-F40MFNT>
2021-08-29 20:03:04 +0900

    요일 입력시 달력 구현

--- src/Jihun/Calendar/Calendar.java
+++ src/Jihun/Calendar/Calendar.java
@@ -4,23 +4,51 @@ import java.util.Scanner;
 
 public class Calendar {
 
-	private static final int[] MAX_DAYS = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+	private static int[] MAX_DAYS = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+	private static int[] LEAP_MAX_DAYS = { 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
 
-	public int getMaxDaysOfMonth(int month) {
-		return MAX_DAYS[month - 1];
+	public boolean isLipYear(int year) {
+		if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0) {
+			return true;
+		} else {
+			return false;
+		}
 	}
-	
 
-	public void printCalendar(int year, int month) {
+	public int getMaxDaysOfMonth(int year, int month, int weekday) {
+		if (isLipYear(year)) {
+			return LEAP_MAX_DAYS[month - 1];
+		} else {
+			return MAX_DAYS[month - 1];
+		}
+	}
+
+	public void printCalendar(int year, int month, int weekday) {
 		System.out.printf("      <<%4d%3d>> \n", year, month);
-		System.out.println(" 일  월  화 수  목  금 토");
+		System.out.println(" SU MO TU WE TH FR SA");
 		System.out.println("----------------------");
-		for(int i=1; i<=getMaxDaysOfMonth(month); i++) {
-			System.out.printf("%3d",i);
-			if (i%7 == 0) {
+		int count = 7 - weekday;
+		int delim = count;
+		if (count < 7) {
+			delim = count;
+		}
+		else {
+			delim = 0;
+		}
+		for (int i = 1; i <= weekday; i++) {
+			System.out.print("   ");
+		}
+		for (int i = 1; i <= count; i++) {
+			System.out.printf("%3d", i);
+		}
+		System.out.println();
+		for (int i = count + 1; i <= getMaxDaysOfMonth(year, month, weekday); i++) {
+			System.out.printf("%3d", i);
+			
+			if (i % 7 == delim) {
 				System.out.println();
 			}
-			if (i == getMaxDaysOfMonth(month)) {
+			if (i == getMaxDaysOfMonth(year, month, weekday)) {
 				System.out.println();
 				System.out.println();
 			}
--- src/Jihun/Calendar/Prompt.java
+++ src/Jihun/Calendar/Prompt.java
@@ -3,22 +3,39 @@ package Jihun.Calendar;
 import java.util.Scanner;
 
 public class Prompt {
-
-	private final static String PROMPT = "cal> ";
+	public int parseDay(String a) {
+		if(a.equals("su")) return 0;
+		else if(a.equals("mo")) return 1;
+		else if(a.equals("tu")) return 2;
+		else if(a.equals("we")) return 3;
+		else if(a.equals("th")) return 4;
+		else if(a.equals("fr")) return 5;
+		else if(a.equals("sa")) return 6;
+		else return 0;
+	}
+	
 
 	public void runPrompt() {
 		// 숫자를 입력받아 입력받은 달의 최대 일수를 구하는 프로그램
 		Calendar cal = new Calendar();
 		Scanner scanner = new Scanner(System.in);
 		while (true) {
+			System.out.println("연도를 입력하세요");
+			System.out.print("YEAR> ");
+			int year = scanner.nextInt();
 			System.out.println("월을 입력하세요");
-			System.out.print(PROMPT);
+			System.out.print("MONTH> ");
 			int month = scanner.nextInt();
+			System.out.println("첫번째 요일을 입력하세요");
+			System.out.print("WEEKDAY> ");
+			String str_weekday = scanner.next();
+			int weekday = parseDay(str_weekday);
+			
 			if (month < 1 || month > 12) {
 				System.out.println("1과 12사이의 수를 입력하세요. Bye~");
 				break;
 			}
-			cal.printCalendar(2021, month);
+			cal.printCalendar(year, month, weekday);
 		}
 		scanner.close();
 	}
