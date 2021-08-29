4a87acc181d91bf20ce71f2e49808731788e0060
지훈 <@DESKTOP-F40MFNT>
2021-08-29 16:17:25 +0900

    PrintCalendar(달력구현) 메소드 추가

--- src/Jihun/Calendar/Calendar.java
+++ src/Jihun/Calendar/Calendar.java
@@ -11,37 +11,19 @@ public class Calendar {
 	}
 	
 
-	public void printSampleCalendar() {
-		System.out.println(" 일 월  화 수  목  금 토");
-		System.out.println("---------------------");
-		System.out.println(" 1  2  3  4  5  6  7");
-		System.out.println(" 8  9 10 11 12 13 14");
-		System.out.println("15 16 17 18 19 20 21");
-		System.out.println("22 23 24 25 26 27 28");
-	}
-
-	public static void main(String[] args) {
-		// 숫자를 입력받아 입력받은 달의 최대 일수를 구하는 프로그램
-		Calendar cal = new Calendar();
-		String Prompt = "Cal> ";
-		Scanner scanner = new Scanner(System.in);
-		System.out.println("반복 횟수를 입력하세요");
-		System.out.print(Prompt);
-		int repeat = scanner.nextInt();
-		int month[] = new int[repeat];
-		System.out.println("월을 입력하세요");
-		for(int i=0; i<month.length; i++) {
-			System.out.print(Prompt);
-			month[i] = scanner.nextInt();
-		}
-		for(int i=0; i<month.length; i++) {
-			if(month[i] < 1 || month[i] > 12) {
-				System.out.println("1과 12사이의 수를 입력하세요");
-				continue;
+	public void printCalendar(int year, int month) {
+		System.out.printf("      <<%4d%3d>> \n", year, month);
+		System.out.println(" 일  월  화 수  목  금 토");
+		System.out.println("----------------------");
+		for(int i=1; i<=getMaxDaysOfMonth(month); i++) {
+			System.out.printf("%3d",i);
+			if (i%7 == 0) {
+				System.out.println();
 			}
-			System.out.printf("%d월달은 %d일까지 있습니다.\n",month[i], cal.getMaxDaysOfMonth(month[i]));
+			if (i == getMaxDaysOfMonth(month)) {
+				System.out.println();
+				System.out.println();
+			}
 		}
-		cal.printSampleCalendar();
-		scanner.close();
 	}
 }
