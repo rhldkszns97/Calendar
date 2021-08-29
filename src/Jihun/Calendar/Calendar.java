3062abccf1c3d2a65b3a01c4ab3b9611796202bb
지훈 <@DESKTOP-F40MFNT>
2021-08-29 12:36:36 +0900

    Add Method getMaxDaysOfMonth, printSampleCalendar

--- src/Jihun/Calendar/Calendar.java
+++ src/Jihun/Calendar/Calendar.java
@@ -2,21 +2,31 @@ package Jihun.Calendar;
 
 import java.util.Scanner;
 
-	public class Calendar {
-	public static void main(String[] args) {
+public class Calendar {
+
+	private static final int[] MAX_DAYS = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+
+	public int getMaxDaysOfMonth(int month) {
+		return MAX_DAYS[month - 1];
+	}
+
+	public void printSampleCalendar() {
 		System.out.println(" 일 월  화 수  목  금 토");
 		System.out.println("---------------------");
 		System.out.println(" 1  2  3  4  5  6  7");
 		System.out.println(" 8  9 10 11 12 13 14");
 		System.out.println("15 16 17 18 19 20 21");
 		System.out.println("22 23 24 25 26 27 28");
-		
-//	 숫자를 입력받아 입력받은 달의 최대 일수를 구하는 프로그램
+	}
+
+	public static void main(String[] args) {
+		// 숫자를 입력받아 입력받은 달의 최대 일수를 구하는 프로그램
+		Calendar cal = new Calendar();
 		Scanner scanner = new Scanner(System.in);
 		System.out.println("달을 입력하세요");
-		int[] monthMaxDays = {31,28,31,30,31,30,31,31,30,31,30,31};
-		int a = scanner.nextInt();
-		System.out.printf("%d월달은 %d일까지 있습니다.\n",a,monthMaxDays[a-1]);
+		int month = scanner.nextInt();
+		System.out.printf("%d월달은 %d일까지 있습니다.\n", month, cal.getMaxDaysOfMonth(month));
+		cal.printSampleCalendar();
 		scanner.close();
 	}
 }
